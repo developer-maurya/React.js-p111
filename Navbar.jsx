16907f5f0f673fb5fba90d7430984a23d676dcac
Chitranjan kumar <chitranjan75075@gmail.com>
2025-11-02 23:32:44 +0530

    Add Navbar component with routing links

--- Navbar.jsx
+++ Navbar.jsx
@@ -1,1 +1,16 @@
+import React from 'react'
+import {NavLink} from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <div className="" id="" style={{backgroundColor: "red" , color: "wheat"}}>
+    <NavLink to ="/home">Home</NavLink>
+    <NavLink to= "/Contact">contact</NavLink>
+
+    </div>
+  )
+}
+
+
+export default Navbar
 
